55c5265b85728a2670bb01e877a2819ee2986f6f
nsmuttoo <nsmuttoo@gmail.com>
2025-04-22 15:20:17 -0400

    wrote calcultor functions in js

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
-console.log("Weeeeee")
+let leftNum =""
+let rightNum =""
+let operator=""
+
+function clear(){
+     leftNum =""
+     rightNum =""
+     operator=""
+}
+
+function number(numb){
+    if(operator == ""){
+        leftNum = leftnum + numb.toString()
+    }
+    rightNum = rightNum+numb.toString()
+}
+
+
+function operate(left,symbol,right){
+    if(symbol == "+"){
+        return add(left,right)
+    }
+
+    if(symbol == "-"){
+        return subtract(left,right)
+    }
+
+    if(symbol == "*"){
+        return multiply(left,right)
+    }
+
+    if(symbol == "/"){
+        if(right == 0){
+            return "ERROR"
+        }
+        return divide(left,right)
+    }
+}
+
+function add(num1,num2){
+    return num1+num2
+}
+
+function subtract(num1,num2){
+    return num1-num2
+}
+
+function multiply(num1,num2){
+    return num1*num2
+}
+
+function divide(num1,num2){
+    return num1/num2
+}
